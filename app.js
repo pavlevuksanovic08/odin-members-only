a79fe7e2532fd3af6b8e46786ecc62498ba67e58
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-08 23:31:21 +0200

    chore: add ejs library and ejs configuration

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 require('dotenv').config()
 const app = express();
 const indexRouter = require('./routes/indexRouter')
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use('/', indexRouter);
 
