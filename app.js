67486c0a35ea18e42404b18f2877f03d8ac86394
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-09 20:51:36 +0200

    feat: add middlewares for auth to app

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
+require('dotenv').config()
+
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const pool = require('./db/pool');
+const bcryptjs = require('bcryptjs');
 
-require('dotenv').config()
 const app = express();
 const indexRouter = require('./routes/indexRouter')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(session({
+    secret: "cats", //TODO
+    resave: false,
+    saveUninitialized: false,
+
+}));
+app.use(passport.session());
+app.use(express.urlencoded({extended: false}));
+
+const strategy = new LocalStrategy(async (username, password, done) => {
+    try {
+        const { rows } = await pool.query(`SELECT * FROM USERS WHERE username = $1;`, [username]);
+        const user = rows[0];
+
+        if (!user) return done(null, false, {message: 'Incorrect username'});
+
+        if (!bcryptjs.compare(password, user.password)) return done(null, false, {message: 'Incorrect password'});
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+})
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+        const user = rows[0];
+
+        done(null, user)
+    } catch (err) {
+        done(err);
+    }
+})
 
 app.use('/', indexRouter);
 
