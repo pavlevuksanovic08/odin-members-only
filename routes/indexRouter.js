30d16315c98cc60bca1ba5a83461f20dc16ea379
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2026-05-14 21:29:08 +0200

    feat: style app

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,8 +5,8 @@ const authController = require('../controllers/authentication');
 const indexController = require('../controllers/index')
 
 router.get('/', indexController.indexController);
-router.get('/sing-up', authController.getSingUp);
-router.post('/sing-up', authController.postSingUp);
+router.get('/sign-up', authController.getSingUp);
+router.post('/sign-up', authController.postSingUp);
 router.get('/log-in', authController.getLogIn);
 router.post('/log-in', authController.postLogIn);
 router.get('/join-the-club', (req, res) => res.render("jointheclub"))
